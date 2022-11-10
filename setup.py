1cf78e817234edcbffaec7f90cb13c60380564a1
odruska <oskar.druska@alumni.fh-aachen.de>
2022-11-10 10:55:32 +0100

    sets SemVer to 1.1.1 (bug fixes)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prometheus_fzj_weather_exporter',
-    version='1.1.0',
+    version='1.1.1',
     description='prometheus exporter for weather data from FZJ',
     author='Oskar Druska',
     author_email='o.druska@fz-juelich.de',
